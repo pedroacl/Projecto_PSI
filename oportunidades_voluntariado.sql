43c59cc482916e359295ce334ed115fb53d5fd7d
Pedro Luís <pedroalexandrecl@gmail.com>
2016-03-19 19:26:44 +0000

    Scripts testados e a funcionar

--- oportunidades_voluntariado.sql
+++ oportunidades_voluntariado.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Oportunidades_Voluntariado (
-    id                  INT AUTO INCREMENT,
+    id                  INT NOT NULL AUTO_INCREMENT,
     id_area_interesse   INT NOT NULL,
     id_grupo_atuacao    INT NOT NULL,
     id_disponibilidade  INT NOT NULL,
@@ -10,10 +10,10 @@ CREATE TABLE Oportunidades_Voluntariado (
     pais                VARCHAR(50) NOT NULL,
     vagas               INT DEFAULT 1,
     ativa               BIT(1) DEFAULT 0,
-    PRIMARY KEY (id)
-    FOREIGN KEY (id_area_interesse) REFERENCES Area_Interesse,
-    FOREIGN KEY (id_grupo_atuacao) REFERENCES Grupo_Atuacao,
-    FOREIGN KEY (id_disponibilidade) REFERENCES Disponibilidade,
-    FOREIGN KEY (id_area_geografica) REFERENCES Area_Geografica,
-    FOREIGN KEY (id_instituicao) REFERENCES Instituicao
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_area_interesse) REFERENCES Areas_Interesse(id),
+    FOREIGN KEY (id_grupo_atuacao) REFERENCES Grupos_Atuacao(id),
+    FOREIGN KEY (id_disponibilidade) REFERENCES Disponibilidades(id),
+    FOREIGN KEY (id_area_geografica) REFERENCES Areas_Geograficas(id),
+    FOREIGN KEY (id_instituicao) REFERENCES Instituicoes(id)
 );
--- periodicidades.sql
+++ periodicidades.sql
@@ -1,8 +1,8 @@
-CREATE TABLE Periodicidade (
-   id                  INT PRIMARY KEY NOT NULL,
+CREATE TABLE Periodicidades (
+   id                  INT NOT NULL AUTO_INCREMENT,
    id_disponibilidade  INT NOT NULL,
-   tipo                VARCHAR(20),
+   tipo                VARCHAR(20) NOT NULL,
    data_fim            DATE NOT NULL,
    PRIMARY KEY (id),
-   FOREIGN KEY (id_disponibilidade) REFERENCES Disponibilidade ON DELETE CASCADE
+   FOREIGN KEY (id_disponibilidade) REFERENCES Disponibilidades(id) ON DELETE CASCADE
 );
