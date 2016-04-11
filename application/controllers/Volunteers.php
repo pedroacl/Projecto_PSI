d6eb3e3187a73d00ace373b4a06466465633e73b
Pedro Luís <pedroalexandrecl@gmail.com>
2016-04-11 19:22:25 +0100

    Adicionada estrutura do CodeIgniter

---
+++ application/controllers/Volunteers.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Volunteers extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('login');
+	}
+
+	public function signup()
+	{
+		$this->load->view('templates/main_template/header');
+		$this->load->view('volunteers/signup');
+		$this->load->view('templates/main_template/footer');
+	}
+}
---
+++ application/views/templates/main_template/header.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../../assets/css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../assets/signin.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <script src="../../../assets/js/jquery-1.12.3.min.js"></script>
+
+    <script src="../../../assets/js/bootstrap/bootstrap.min.js"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">About</a></li>
+              <li class="dropdown">
+                  <a class="dropdown-toggle" data-toggle="dropdown" href="#">My Other Pets
+                  <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">My Pet 1</a></li>
+                    <li><a href="#">My Pet 2</a></li>
+                  </ul>
+              </li>
+          </ul>
+        </div>
+      </nav>
+
+
+
+
