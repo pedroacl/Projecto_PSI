452f366105c166e5bbe8cf99847bef3a30dbcfd7
Pedro Luís <pedroalexandrecl@gmail.com>
2016-04-22 15:56:08 +0100

    Alterada seleçao do tipo de user

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-	$('.volunteer_fields').hide();
-	$('.institution_fields').hide();
+	$volunteer_div = $('.volunteer_fields');
+	$institution_div = $('.institution_fields');
+
+	$volunteer_fields = $volunteer_div.remove();
+	$institution_fileds = $institution_div.remove();
 
 	$('#default_user_select').click(function() {
 		$('.volunteer_fields').hide("slow");
@@ -8,18 +11,17 @@ $(document).ready(function() {
 	});
 
 	$('#institution_select').click(function() {
-		$('.volunteer_fields').hide();
-		$('.institution_fields').show("slow");
+		$volunteer_div.remove();
+		$('#user_form_end').append($institution_fileds);
 	});
 
 	$('#volunteer_select').click(function() {
-		$('.volunteer_fields').show("slow");
+		$institution_div.remove();
+		$('#user_form_end').append($volunteer_fields);
 	});
 
 	$('.datepicker').datepicker({
     	format: 'mm/dd/yyyy',
    	startDate: '-3d'
 	});
-
-	alert($('.datepicker').html());
 });
