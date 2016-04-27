9ef25b360c41585fa3ce2ba787eeb00000598aea
Pedro Luís <pedroalexandrecl@gmail.com>
2016-04-27 15:13:35 +0100

    Adicionado JSON de distritos

---
+++ assets/js/areas_geograficas.js
@@ -0,0 +1,68 @@
+var areas_geograficas = {
+	"distritos": [
+	{
+		"nome"      : "Lisboa",
+		"concelhos" : [
+			{
+				"nome"      : "Amadora"
+				"freguesias": [
+					{ "nome" : "Águas Livres" },
+					{ "nome" : "Alfragide" },
+					{ "nome" : "Encosta do Sol" }
+				]
+			},
+			{
+				"nome"      : "Cascais",
+				"freguesias": [
+					{ "nome" : "Alcabideche" },
+					{ "nome" : "Carcavelos e Parede" },
+					{ "nome" : "Cascais e Estoril" }
+				]
+			}
+		]
+	},
+
+	{
+		"nome"      : "Portalegre",
+		"concelhos" : [
+			{
+				"nome"      : "Avis"
+				"freguesias": [
+					{ "nome" : "Aldeia Velha" },
+					{ "nome" : "Avis" },
+					{ "nome" : "Figueira e Barros" }
+				]
+			},
+			{
+				"nome"      : "Campo Maior",
+				"freguesias": [
+					{ "nome" : "Nossa Senhora da Expectação" },
+					{ "nome" : "Nossa Senhora da Graça dos Degolados" },
+					{ "nome" : "São João Baptista" }
+				]
+			}
+		]
+	},
+
+	{
+		"nome"      : "Porto",
+		"concelhos" : [
+			{
+				"nome"      : "Amarante"
+				"freguesias": [
+					{ "nome" : "Amarante" },
+					{ "nome" : "Gondar" },
+					{ "nome" : "Mancelos" }
+				]
+			},
+			{
+				"nome"      : "Porto",
+				"freguesias": [
+					{ "nome" : "Aldoar" },
+					{ "nome" : "Bonfim" },
+					{ "nome" : "Paranhos" }
+				]
+			}
+		]
+	}]
+}
