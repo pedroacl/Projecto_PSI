804db90ccb9277c08ac6f6113e65e33d8b324f02
Pedro Luís <pedroalexandrecl@gmail.com>
2016-04-28 17:49:56 +0100

    Atualizado ficheiro oracle com inserts

--- creates_oracle.sql
+++ creates_oracle.sql
@@ -27,57 +27,6 @@ END;
 /
 
 
-CREATE TABLE Voluntarios (
-   id                          NUMBER(10)          ,
-   id_area_geografica          NUMBER(10)           NOT NULL,
-   id_habilitacoes_academicas  NUMBER(10)           NOT NULL,
-   id_utilizador               NUMBER(10)           NOT NULL,
-   genero                      CHAR(1)       NOT NULL,
-   data_nascimento             DATE          NOT NULL,
-   foto                        VARCHAR2(100)  DEFAULT NULL,
-   PRIMARY KEY (id),
-   FOREIGN KEY (id_area_geografica)          REFERENCES Areas_Geograficas(id),
-   FOREIGN KEY (id_habilitacoes_academicas)  REFERENCES Habilitacoes_Academicas(id),
-   FOREIGN KEY (id_utilizador)               REFERENCES Utilizadores(id)
-);
-
--- Generate ID using sequence and trigger
-CREATE SEQUENCE Voluntarios_seq START WITH 1 INCREMENT BY 1;
-
-CREATE OR REPLACE TRIGGER Voluntarios_seq_tr
- BEFORE INSERT ON Voluntarios FOR EACH ROW
- WHEN (NEW.id IS NULL)
-BEGIN
- SELECT Voluntarios_seq.NEXTVAL INTO :NEW.id FROM DUAL;
-END;
-/
-
-
-CREATE TABLE Instituicoes (
-   id                   NUMBER(10)         ,
-   id_area_geografica   NUMBER(10)          NOT NULL,
-   id_utilizador        NUMBER(10)          NOT NULL,
-   descricao            VARCHAR2(50)  NOT NULL,
-   morada               VARCHAR2(50)  NOT NULL,
-   email_instituicao    VARCHAR2(20)  NOT NULL,
-   website              VARCHAR2(20)  DEFAULT NULL,
-   PRIMARY KEY (id),
-   FOREIGN KEY (id_area_geografica) REFERENCES Areas_Geograficas(id),
-   FOREIGN KEY (id_utilizador) REFERENCES Utilizadores(id)
-);
-
--- Generate ID using sequence and trigger
-CREATE SEQUENCE Instituicoes_seq START WITH 1 INCREMENT BY 1;
-
-CREATE OR REPLACE TRIGGER Instituicoes_seq_tr
- BEFORE INSERT ON Instituicoes FOR EACH ROW
- WHEN (NEW.id IS NULL)
-BEGIN
- SELECT Instituicoes_seq.NEXTVAL INTO :NEW.id FROM DUAL;
-END;
-/
-
-
 CREATE TABLE Utilizadores_Grupos_Atuacao (
    id_utilizador        NUMBER(10),
    id_grupo_atuacao     NUMBER(10),
@@ -189,6 +138,26 @@ END;
 /
 
 
+
+CREATE TABLE Tipos_Habilitacoes_Academicas (
+   id          NUMBER(10)              ,
+   nome        VARCHAR2(50)       NOT NULL UNIQUE,
+   descricao   VARCHAR2(200),
+   PRIMARY KEY (id)
+);
+
+-- Generate ID using sequence and trigger
+CREATE SEQUENCE Tipos_Habilitacoes_Academicas_seq START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER Tipos_Habilitacoes_Academicas_seq_tr
+ BEFORE INSERT ON Tipos_Habilitacoes_Academicas FOR EACH ROW
+ WHEN (NEW.id IS NULL)
+BEGIN
+ SELECT Tipos_Habilitacoes_Academicas_seq.NEXTVAL INTO :NEW.id FROM DUAL;
+END;
+/
+
+
 CREATE TABLE Habilitacoes_Academicas (
    id                   NUMBER(10)     ,
    id_tipo              NUMBER(10)         NOT NULL,
@@ -211,21 +180,55 @@ END;
 /
 
 
-CREATE TABLE Tipos_Habilitacoes_Academicas (
-   id          NUMBER(10)              ,
-   nome        VARCHAR2(50)       NOT NULL UNIQUE,
-   descricao   VARCHAR2(200),
-   PRIMARY KEY (id)
+
+
+CREATE TABLE Voluntarios (
+   id                          NUMBER(10)          ,
+   id_area_geografica          NUMBER(10)           NOT NULL,
+   id_habilitacoes_academicas  NUMBER(10)           NOT NULL,
+   id_utilizador               NUMBER(10)           NOT NULL,
+   genero                      CHAR          NOT NULL,
+   data_nascimento             DATE          NOT NULL,
+   foto                        VARCHAR2(100)  DEFAULT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (id_area_geografica)          REFERENCES Areas_Geograficas(id),
+   FOREIGN KEY (id_habilitacoes_academicas)  REFERENCES Habilitacoes_Academicas(id),
+   FOREIGN KEY (id_utilizador)               REFERENCES Utilizadores(id)
 );
 
 -- Generate ID using sequence and trigger
-CREATE SEQUENCE Tipos_Habilitacoes_Academicas_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE Voluntarios_seq START WITH 1 INCREMENT BY 1;
 
-CREATE OR REPLACE TRIGGER Tipos_Habilitacoes_Academicas_seq_tr
- BEFORE INSERT ON Tipos_Habilitacoes_Academicas FOR EACH ROW
+CREATE OR REPLACE TRIGGER Voluntarios_seq_tr
+ BEFORE INSERT ON Voluntarios FOR EACH ROW
  WHEN (NEW.id IS NULL)
 BEGIN
- SELECT Tipos_Habilitacoes_Academicas_seq.NEXTVAL INTO :NEW.id FROM DUAL;
+ SELECT Voluntarios_seq.NEXTVAL INTO :NEW.id FROM DUAL;
+END;
+/
+
+
+CREATE TABLE Instituicoes (
+   id                   NUMBER(10)         ,
+   id_area_geografica   NUMBER(10)          NOT NULL,
+   id_utilizador        NUMBER(10)          NOT NULL,
+   descricao            VARCHAR2(50)  NOT NULL,
+   morada               VARCHAR2(50)  NOT NULL,
+   email_instituicao    VARCHAR2(20)  NOT NULL,
+   website              VARCHAR2(20)  DEFAULT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (id_area_geografica) REFERENCES Areas_Geograficas(id),
+   FOREIGN KEY (id_utilizador) REFERENCES Utilizadores(id)
+);
+
+-- Generate ID using sequence and trigger
+CREATE SEQUENCE Instituicoes_seq START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER Instituicoes_seq_tr
+ BEFORE INSERT ON Instituicoes FOR EACH ROW
+ WHEN (NEW.id IS NULL)
+BEGIN
+ SELECT Instituicoes_seq.NEXTVAL INTO :NEW.id FROM DUAL;
 END;
 /
 
@@ -293,3 +296,40 @@ CREATE TABLE ci_sessions (
 );
 
 CREATE INDEX ci_sessions_timestamp ON ci_sessions (timestamp);
+
+
+-- Inserts
+ALTER SESSION SET CURRENT_SCHEMA = PSI;
+
+INSERT INTO Areas_Geograficas (freguesia, concelho, distrito)
+VALUES ('Campo Grande', 'Lisboa', 'Lisboa');
+
+INSERT INTO Areas_Geograficas (freguesia, concelho, distrito)
+VALUES ('Leiria', 'Leiria', 'Leiria');
+
+
+INSERT INTO Areas_Interesse (nome)
+VALUES ('Saude');
+
+INSERT INTO Areas_Interesse (nome)
+VALUES ('Educação');
+
+INSERT INTO Areas_Interesse (nome)
+VALUES ('Desporto');
+
+
+INSERT INTO Grupos_Atuacao (nome, descricao)
+VALUES ('Idosos', 'Grupo de pessoas idosas');
+
+INSERT INTO Grupos_Atuacao (nome, descricao)
+VALUES ('Crianças', 'Grupos de crianças');
+
+
+INSERT INTO Tipos_Habilitacoes_Academicas (nome, descricao)
+VALUES ('Licenciatura', 'Grau de licenciado');
+
+INSERT INTO Tipos_Habilitacoes_Academicas (nome, descricao)
+VALUES ('Mestrado', 'Grau de mestre');
+
+INSERT INTO Tipos_Habilitacoes_Academicas (nome, descricao)
+VALUES ('Doutoramento', 'Grau de doutorado');
