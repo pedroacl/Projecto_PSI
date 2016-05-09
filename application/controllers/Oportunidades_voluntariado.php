5870623496678ad6d643a52375f6a0287ba85353
Pedro Luís <pedroalexandrecl@gmail.com>
2016-05-09 15:06:01 +0100

    Adicionado controller de oportunidades de voluntariado

--- application/controllers/Oportunidades_voluntariado.php
+++ application/controllers/Oportunidades_voluntariado.php
@@ -7,13 +7,13 @@ class Oportunidades_voluntariado extends MY_Controller {
 		parent::__construct();
 
 		$this->authenticate_user();
+		$this->validate_is_instituicao();
 		$this->load->model('Oportunidade_voluntariado', 'oportunidade_voluntariado');
 	}
 
 	public function index()
 	{
 		$this->load->library('session');
-		$this->validate_is_instituicao();
 
 		$this->load->view('templates/main_template/header');
 		$this->load->view('/oportunidades_voluntariado/index');
